39617c29406cda231c84b5019efdcca5a80bcce6
HongLabInc <95195816+HongLabInc@users.noreply.github.com>
2024-02-18 21:29:12 -0800

    챕터1 VSCode용 코드

---
+++ VSCode/Ex0102_Subtraction/Ex0102_Subtraction.cpp
@@ -0,0 +1,64 @@
+﻿#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+// 항상 큰 수에서 작은 수를 빼는 경우(결과가 음수가 되지 않는 경우)를 가정
+string Subtract(string str1, string str2)
+{
+	// 둘이 같을 경우 바로 "0" 반환
+	if (str1 == str2)
+		return "0"; // '0'은 char, "0"은 string
+
+	int N = max(str1.size(), str2.size());
+	str1 = string(N - str1.size(), '0') + str1; // 문자열끼리의 더하기도 가능
+	str2 = string(N - str2.size(), '0') + str2;
+
+	string result(N, '0');
+
+	// TODO: 더하기와 거의 비슷합니다.
+
+	// 불필요한 '0' 제거 (예: "078" -> "78")
+	// TODO:
+
+	return result;
+}
+
+int main()
+{
+	// 항상 큰 수에서 작은 수를 빼는 경우(결과가 음수가 되지 않는 경우)를 가정
+	vector<vector<string>> tests = {
+		{"34", "12", std::to_string(34 - 12)}
+		, {"123", "45", std::to_string(123 - 45)}
+		, {"54544", "44545", std::to_string(54544 - 44545)}
+		, {"5555", "55", std::to_string(5555 - 55)}
+		, {"5555", "5555", std::to_string(5555 - 5555)}
+		, {"9823471235421415454545454545454544", "1714546546546545454544548544544545", "8108924688874870000000906000909999"}
+	};
+
+	for (const auto& t : tests)
+	{
+		const string str1 = t[0];
+		const string str2 = t[1];
+		const string expected = t[2];
+
+		cout << str1 << " - " << str2 << " = " << expected << endl;
+
+		const string result = Subtract(str1, str2);
+
+		cout << result << " " << expected << " ";
+
+		if (result == expected)
+			cout << "OK";
+		else {
+			cout << "Not OK";
+			exit(-1);
+		}
+		cout << endl << endl;
+	}
+
+	cout << "Congratulations. All OK!" << endl;
+
+	return 0;
+}
---
+++ VSCode/Ex0103_Multiplication/Ex0103_Multiplication.cpp
@@ -0,0 +1,49 @@
+﻿#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+string Multiply(string str1, string str2)
+{
+	// TODO:
+
+	return "0";
+}
+
+int main()
+{
+	vector<vector<string>> tests = {
+		{"12", "34", std::to_string(12 * 34)},
+		{"123", "45", std::to_string(123 * 45)},
+		{"5555", "55", std::to_string(5555 * 55)},
+		{"5555", "5555", std::to_string(5555 * 5555)},
+		{"98234712354214154", "171454654654655", "16842798681791158832220782986870"}
+		// , {"9823471235421415454545454545454544", "1714546546546545454544548544544545", "16842798681791114273590624445460185389471221520083884298838480662480"}
+	};
+
+	for (const auto& t : tests)
+	{
+		const string str1 = t[0];
+		const string str2 = t[1];
+		const string expected = t[2];
+
+		cout << str1 << " * " << str2 << " = " << expected << endl;
+
+		const string result = Multiply(str1, str2);
+
+		cout << result << " " << expected << " ";
+
+		if (result == expected)
+			cout << "OK";
+		else {
+			cout << "Not OK";
+			exit(-1);
+		}
+		cout << endl << endl;
+	}
+
+	cout << "Congratulations. All OK!" << endl;
+
+	return 0;
+}
