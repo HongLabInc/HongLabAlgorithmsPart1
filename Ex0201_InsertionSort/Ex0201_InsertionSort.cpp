99e5e5f486ad5b1eed9e24d606b8016593fb9b7b
HongLabInc <95195816+HongLabInc@users.noreply.github.com>
2024-02-22 23:31:11 -0800

    Ex0201_InsertionSort

---
+++ Ex0201_InsertionSort/Ex0201_InsertionSort.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+bool CheckSorted(vector<int>& arr)
+{
+	for (int i = 0; i < arr.size() - 1; i++)
+	{
+		if (arr[i] > arr[i + 1])
+			return false;
+	}
+
+	return true;
+}
+
+void Print(vector<int>& arr)
+{
+	for (auto v : arr)
+		cout << v << " ";
+	cout << endl;
+}
+
+int main()
+{
+	// vector<int> a = { 6, 5, 4, 3, 2, 1 };
+	vector<int> a = { 6, 4, 3, 8, 5 };
+
+	int N = a.size();
+	for (int i = 1; i < N; i++)
+	{
+		// TODO: 딱 2줄만 사용
+
+		Print(a);
+	}
+}
+
