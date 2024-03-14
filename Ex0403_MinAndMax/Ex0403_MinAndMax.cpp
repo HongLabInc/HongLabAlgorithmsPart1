17d37db3bbed6a8cf4ddd2863d6e6f158a596749
HongLabInc <95195816+HongLabInc@users.noreply.github.com>
2024-03-13 17:49:35 -0700

    Ex0403_MinAndMax 문제

---
+++ Ex0403_MinAndMax/Ex0403_MinAndMax.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	vector<int> arr = { 8, 2, 3, 5, 9, 1, 9, 4, 3, 7, 6, 7 };
+
+	// TODO:
+
+	cout << "Min value = " << arr[0] << ", Max value = " << arr[1] << endl; // Min value = 1, Max value = 9
+}
