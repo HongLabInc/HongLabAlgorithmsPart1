65c1acfb06d36565029c67cb69ece6ba16c13a0f
HongLabInc <95195816+HongLabInc@users.noreply.github.com>
2024-04-01 15:55:36 -0700

    Ex0803_RomanNumeral (Leetcode problem)

---
+++ Ex0803_RomanNumeral/Ex0803_RomanNumeral.cpp
@@ -0,0 +1,60 @@
+﻿#include <iostream>
+#include <string>
+#include <unordered_map>
+using namespace std;
+
+// LeetCode: Roman to Integer
+// https://leetcode.com/problems/roman-to-integer/description/
+
+int RomanToInt(string s)
+{
+	unordered_map<char, int> m;
+
+	m['I'] = 1;
+	m['V'] = 5;
+	m['X'] = 10;
+	m['L'] = 50;
+	m['C'] = 100;
+	m['D'] = 500;
+	m['M'] = 1000;
+
+	int ans = 0;
+
+	for (int i = 0; i < s.length(); i++)
+	{
+		// TODO:
+	}
+
+	cout << s << " = " << ans << endl;
+
+	return ans;
+}
+
+int main()
+{
+	RomanToInt("II");
+
+	RomanToInt("III");
+
+	RomanToInt("XII");
+
+	RomanToInt("XXVII");
+
+	RomanToInt("IV");
+
+	RomanToInt("IX");
+
+	RomanToInt("XL");
+
+	RomanToInt("XC");
+
+	RomanToInt("CD");
+
+	RomanToInt("CM");
+
+	RomanToInt("LVIII");
+
+	RomanToInt("MCMXCIV");
+
+	return 0;
+}
