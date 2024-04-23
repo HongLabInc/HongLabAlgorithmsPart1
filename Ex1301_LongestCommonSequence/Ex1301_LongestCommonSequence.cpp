905ee9b1eccb4f6058a65a58e86d4ec34e948cb9
HongLabInc <95195816+HongLabInc@users.noreply.github.com>
2024-04-23 15:12:25 -0700

    Update Ex1301

--- Ex1301_LongestCommonSequence/Ex1301_LongestCommonSequence.cpp
+++ Ex1301_LongestCommonSequence/Ex1301_LongestCommonSequence.cpp
@@ -5,44 +5,42 @@
 #include <iomanip>
 using namespace std;
 
-// LCS 문자열을 반환하는 함수
-// 왼쪽부터 비교하는 구현 방식
-string RecurLCSLeftFirst(const string& X, const string& Y, int i, int j)
+// X에서 첫 m글자, Y에서 첫 n글자에 대해 LCS를 찾는 함수
+string RecurLCS(const string& X, const string& Y, int m, int n)
 {
-	if (i == X.size() || j == Y.size())
+	if (m == 0 || n == 0)
 		return string("");
 
-	//if (X[i] == Y[j])
-	//{
-	//	return TODO;
-	//}
-	//else
-	//{
-	//	string s1 = TODO;
-	//	string s2 = TODO;
-	//	return s1.size() > s2.size() ? s1 : s2;
-	//}
+	/*
+	if (X[m - 1] == Y[n - 1])
+	{
+		return TODO:
+	}
+	else
+	{
+		string s1 = TODO:
+		string s2 = TODO:
 
-	return string(""); // TODO: 삭제
+		return s1.size() >= s2.size() ? s1 : s2;
+	}
+	*/
+
+	return string(""); // TODO: 불필요, 삭제
 }
 
-// 최대 길이만 반환하는 함수
-// 오른쪽부터 비교하는 구현 방식
 int RecurLengthLCS(const string& X, const string& Y, int m, int n)
 {
 	if (m == 0 || n == 0)
 		return 0;
 
-	//if (X[m - 1] == Y[n - 1])
-	//{
-	//	return TODO;
-	//}
-	//else
-	//{
-	//	return TODO;
-	//}
+	/*
+	if (X[m - 1] == Y[n - 1])
+		return TODO
+	else
+		return max(TODO, TODO);
+	*/
 
-	return 0; // TODO: 삭제
+	return 0; // TODO: 불필요, 삭제
 }
 
 void PrintLCS(const vector<vector<char>>& prev, const string& X, int i, int j)
@@ -50,16 +48,16 @@ void PrintLCS(const vector<vector<char>>& prev, const string& X, int i, int j)
 	if (i == 0 || j == 0)
 		return;
 
-	if (prev[i][j] == '\\') // 왼쪽 위로 올라가는 대각선 화살표를 위미
+	if (prev[i][j] == '\\')
 	{
 		PrintLCS(prev, X, i - 1, j - 1);
 		cout << X[i - 1]; // 주의: -1
 	}
-	else if (prev[i][j] == '|') // 위로 올라가는 화살표를 의미
+	else if (prev[i][j] == '|')
 	{
 		PrintLCS(prev, X, i - 1, j);
 	}
-	else // '-'는 왼쪽으로 가는 화살표를 의미
+	else
 	{
 		PrintLCS(prev, X, i, j - 1);
 	}
@@ -67,35 +65,47 @@ void PrintLCS(const vector<vector<char>>& prev, const string& X, int i, int j)
 
 int BottomUpLCS(const string& X, const string& Y, int m, int n)
 {
-	vector<vector<int>> table(m + 1, vector<int>(n + 1, 0)); // LCS의 길이만 저장
-	vector<vector<string>> lcs(m + 1, vector<string>(n + 1, "")); // LCS 문자열 저장
+	// 주의: 문자열과 table의 인덱싱에 일관성이 없어서 헷갈릴 수 있습니다.
+	//       table은 1-base 인덱싱, X와 Y는 0-base 인덱싱
+	//       table[i][j]: X의 길이가 i이고 Y의 길이가 j인 경우를 의미
+	//       table에서 1-base를 사용하는 이유는 i나 j가 0인 base case들도 포함시키기 위해서 입니다.
 
+	// 문제풀이할때는 보통 table 채워서 최대 길이를 찾는 것이 목표입니다.
+	// 추가적으로 prev와 lcs를 제공하는 이유는 이해를 돕기 위해서 입니다.
+	vector<vector<int>> table(m + 1, vector<int>(n + 1, 0));
 	vector<vector<char>> prev(m + 1, vector<char>(n + 1, ' '));
-	// prev에는 화살표를 문자로 저장
-	// PrintLCS() 참고
 
+	vector<vector<string>> lcs(m + 1, vector<string>(n + 1, ""));
+	// prev에는 강의 노트의 화살표를 문자로 저장, PrintLCS() 참고
+
+	// 여기서 i, j는 X와 Y의 길이를 의미 (1-base)
 	for (int i = 1; i <= m; i++)
 	{
 		for (int j = 1; j <= n; j++)
 		{
+			/*
 			if (X[i - 1] == Y[j - 1]) // 주의: i - 1, j - 1
 			{
-				//table[i][j] = TODO;
-				//prev[i][j] = '\\'; // [i-1][j-1] -> [i][j]라는 기록
-				//lcs[i][j] = TODO;
+				table[i][j] = TODO;
+
+				prev[i][j] = '\\'; // [i-1][j-1]에서 가져왔다는 의미
+				lcs[i][j] = TODO;
 			}
 			else if (table[i - 1][j] >= table[i][j - 1])
 			{
-				//table[i][j] = TODO;
-				//prev[i][j] = '|'; // [i-1][j] -> [i][j]라는 기록
-				//lcs[i][j] = TODO;
+				table[i][j] = TODO;
+
+				prev[i][j] = '|'; // [i-1][j]에서 가져왔다는 의미
+				lcs[i][j] = TODO;
 			}
 			else
 			{
-				//table[i][j] = TODO;
-				//prev[i][j] = '-'; // [i][j - 1] -> [i][j]라는 기록
-				//lcs[i][j] = TODO;
+				table[i][j] = TODO;
+
+				prev[i][j] = '-'; // [i][j-1]에서 가져왔다는 의미
+				lcs[i][j] = TODO;
 			}
+			*/
 		}
 	}
 
@@ -130,7 +140,7 @@ int BottomUpLCS(const string& X, const string& Y, int m, int n)
 	}
 	cout << endl;
 
-	return table[m][n];
+	return table[m][n]; // 결과
 }
 
 int main()
@@ -144,13 +154,12 @@ int main()
 	//string Y = "GTCGTTCGGAATGCCGTTGCTCTGTAAA";
 	// GTCGTCGGAAGCCGGCCGAA
 
-	//위 예시의 일부
 	//string X = "ACCGGTCGAGT";
 	//string Y = "GTCGTTCGGAA";
 	// CGTCGA
 	//6
 
-	cout << RecurLCSLeftFirst(X, Y, 0, 0) << endl;
+	cout << RecurLCS(X, Y, 0, 0) << endl;
 	cout << RecurLengthLCS(X, Y, int(X.size()), int(Y.size())) << endl;
 	cout << BottomUpLCS(X, Y, int(X.size()), int(Y.size())) << endl;
 
